2bcc4a89c6ca98e3ff23c25a94a062894ced0866
aramrw <106574385+aramrw@users.noreply.github.com>
2024-02-14 23:15:52 -0800

    Loading States + Error Handling

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,7 +5,7 @@ import React, { useEffect, useState } from 'react'
 import { open } from '@tauri-apps/api/dialog';
 import { addFolder, deleteFolder, getFolders, getUsers } from '../../../lib/prisma-commands';
 import type { User } from "@prisma/client";
-import { Folder, Trash2 } from 'lucide-react';
+import { Folder, Loader, Trash2 } from 'lucide-react';
 import { FileEntry, readDir } from '@tauri-apps/api/fs'
 import { cn } from '@/lib/utils';
 import { invoke } from '@tauri-apps/api/tauri';
@@ -70,12 +70,13 @@ export default function Dashboard() {
         )
     }
 
-    function FolderList({ folderPath, asChild }: { folderPath: string, asChild?: boolean | undefined }): JSX.Element {
+    function FolderList({ folderPath, asChild }: { folderPath: string, asChild?: boolean | undefined }) {
 
         let [files, setFiles] = useState<FileEntry[]>([]);
         let [folders, setFolders] = useState<FileEntry[]>([]);
         let [expanded, setExpanded] = useState(false);
         let [subtitleFiles, setSubtitleFiles] = useState<FileEntry[]>([]);
+        let [deleting, setDeleting] = useState(false);
 
         useEffect(() => {
             readDir(folderPath).then((res) => {
@@ -111,102 +112,121 @@ export default function Dashboard() {
             })
         }, [])
 
-        return (
-            <main className='h-full w-full overflow-hidden'>
-                <motion.div className={cn('flex cursor-pointer flex-row items-center justify-between rounded-md bg-accent p-1 shadow-sm',
-                    (expanded && files.length > 0 && !asChild) && 'rounded-b-none border-b-4 border-tertiary',
-                    (expanded && folders.length > 0 && !asChild) && 'rounded-b-none border-b-4 border-tertiary',
-                    asChild && ' px-1 border-none py-0'
-                )}
-                    onClick={() => {
-                        setExpanded(!expanded);
-                    }}
-                    whileHover={{ scale: 0.98 }}
-                    transition={{ duration: 0.15 }}
-                >
-                    <div className='flex flex-row items-center justify-center gap-1 text-lg font-medium text-primary'>
-                        {asChild && <Folder size={20} />}
-                        {folderPath.replace(/\\/g, '/').split('/').pop()}
-                    </div>
-                    {asChild !== true && (
-                        <motion.span
-                            whileHover={{ scale: 1.1 }}
-                            whileTap={{ scale: 0.9 }}
-                            className=''
-                        >
-                            <Trash2 className='rounded-lg p-0.5 text-destructive hover:bg-white' onClick={() => {
-                                setExpanded(false);
-                                // trigger the delete folder db command
-                                deleteFolder({ folderPath }).then(() => {
-                                    window.location.reload();
-                                });
-                            }} />
-
-                        </motion.span>
+        if (!deleting) {
+            return (
+                <main className='h-full w-full overflow-hidden'>
+                    <motion.div className={cn('flex cursor-pointer flex-row items-center justify-between rounded-md bg-accent p-1 shadow-sm',
+                        (expanded && files.length > 0 && !asChild) && 'rounded-b-none border-b-4 border-tertiary',
+                        (expanded && folders.length > 0 && !asChild) && 'rounded-b-none border-b-4 border-tertiary',
+                        (asChild && expanded) && ' px-1 border-none my-1.5'
                     )}
-                </motion.div>
-                <ul className='overflow-hidden overflow-y-auto bg-muted'>
-                    {
-                        expanded && files.map((file, index) => {
-                            return (
-                                <motion.li className={cn('flex flex-col items-start justify-center gap-1 border-b-2 p-0.5 px-4 cursor-pointer overflow-hidden',
-                                    (index === files.length - 1 && !asChild) && 'rounded-b-md border-b-4 border-tertiary',
-                                )}
-                                    onClick={() => {
-                                        // open the file in the default video player
-                                        invoke('open_video', { path: file.path });
+                        onClick={() => {
+                            setExpanded(!expanded);
+                        }}
+                        whileHover={{ scale: 0.98 }}
+                        transition={{ duration: 0.15 }}
+                    >
+                        <div className={cn('flex flex-row items-center justify-center gap-1 text-lg font-medium text-primary',
 
-                                    }}
-                                    key={index}
 
-                                    initial={{ scale: 0.8 }}
-                                    animate={{ scale: 1 }}
-                                    exit={{ scale: 0 }}
-                                    whileHover={{ x: 10 }}
-                                >
-                                    {!file.children && <span>{file.name}</span>}
-                                </motion.li>
+                        )}>
+                            {asChild && <Folder size={20} />}
+                            {folderPath.replace(/\\/g, '/').split('/').pop()}
+                        </div>
+                        {asChild !== true && (
+                            <motion.span
+                                whileHover={{ scale: 1.1 }}
+                                whileTap={{ scale: 0.9 }}
+                                className=''
+                            >
+                                <Trash2 className='rounded-lg p-0.5 text-destructive hover:bg-white' onClick={() => {
+                                    setDeleting(true);
+                                    // trigger the delete folder db command
+                                    deleteFolder({ folderPath }).then(() => {
+                                        window.location.reload();
+                                    });
+                                }} />
 
-                            )
-                        })
-                    }
-                    {
-                        expanded && folders.map((folder, index) => {
-                            return (
-                                <motion.li className={cn('flex flex-col items-start justify-center gap-1 border-b-2 p-0.5 px-2 cursor-pointer overflow-hidden',
-                                    (index === files.length - 1 && !asChild) && 'rounded-b-md border-b-4 border-tertiary',
-                                    (index === folders.length - 1 && !asChild) && 'rounded-b-md border-b-4 border-tertiary',
-                                    asChild && 'px-3.5 border-none my-1.5',
-                                )}
-                                    key={index}
+                            </motion.span>
+                        )}
+                    </motion.div>
+                    <ul className='overflow-hidden overflow-y-auto bg-muted'>
+                        {
+                            expanded && files.map((file, index) => {
+                                return (
+                                    <motion.li className={cn('flex flex-col items-start justify-center gap-1 border-b-2 p-0.5 px-4 cursor-pointer overflow-hidden',
+                                        (index === files.length - 1 && !asChild) && 'rounded-b-md border-b-4 border-tertiary',
+                                    )}
+                                        onClick={() => {
+                                            // open the file in the default video player
+                                            invoke('open_video', { path: file.path });
 
-                                    initial={{ scale: 0.8 }}
-                                    animate={{ scale: 1 }}
-                                    exit={{ scale: 0 }}
-                                    whileHover={{ x: 10 }}
-                                >
+                                        }}
+                                        key={index}
 
-                                    <FolderList folderPath={folder.path} asChild />
+                                        initial={{ scale: 0.8 }}
+                                        animate={{ scale: 1 }}
+                                        exit={{ scale: 0 }}
+                                        whileHover={{ x: 10 }}
+                                    >
+                                        {!file.children && <span>{file.name}</span>}
+                                    </motion.li>
 
-                                </motion.li>
+                                )
+                            })
+                        }
+                        {
+                            expanded && folders.map((folder, index) => {
+                                return (
+                                    <motion.li className={cn('flex flex-col items-start justify-center gap-1 border-b-2 p-0.5 px-2 cursor-pointer overflow-hidden',
+                                        (index === files.length - 1 && !asChild) && 'rounded-b-md border-b-4 border-tertiary',
+                                        (index === folders.length - 1 && !asChild) && 'rounded-b-md border-b-4 border-tertiary',
+                                        asChild && 'px-3.5 border-none my-1',
+                                    )}
+                                        key={index}
 
-                            )
-                        })
-                    }
-                </ul>
+                                        initial={{ scale: 0.8 }}
+                                        animate={{ scale: 1 }}
+                                        exit={{ scale: 0 }}
+                                        whileHover={{ x: 10 }}
+                                    >
+
+                                        <FolderList folderPath={folder.path} asChild />
+
+                                    </motion.li>
+
+                                )
+                            })
+                        }
+                    </ul>
+                </main>
+            )
+        } else {
+            return (
+                <Loader className='animate-spin' />
+            )
+        }
+    }
+
+    if (currentUser) {
+        return (
+            <main className='flex h-fit w-full flex-col gap-2 p-3'>
+                {folderPaths.map((folder, index) => {
+                    return <FolderList folderPath={folder} key={index} />
+                })}
+
+                <AddFolder />
+            </main>
+        )
+    } else {
+        return (
+            <main className='flex h-1/2 w-full flex-row items-center justify-center'>
+                <Loader className='animate-spin' size={40} />
             </main>
         )
     }
 
-    return (
-        <main className='flex h-fit w-full flex-col gap-2 p-3'>
-            {folderPaths.map((folder, index) => {
-                return <FolderList folderPath={folder} key={index} />
-            })}
 
-            <AddFolder />
-        </main>
-    )
 }
 
 
