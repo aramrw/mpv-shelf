cce87c52de5c7d25fd212018562c9b9c86730cec
aramrw <106574385+aramrw@users.noreply.github.com>
2024-02-24 11:58:37 -0800

    update version 0.3.2

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,13 +9,15 @@ use serde::{Deserialize, Serialize};
 
 use std::collections::HashMap;
 use std::io::{stdout, Write};
-// use std::os::windows::process;
 use std::process::Command;
+// use std::os::windows::process;
 use std::vec;
 use sysinfo::System;
-use tauri::{generate_handler, Manager, Window};
 #[allow(unused_imports)]
-use tauri::{CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem};
+use tauri::{
+    command, CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem,
+};
+use tauri::{generate_handler, Manager, Window};
 
 #[derive(Serialize, Deserialize)]
 struct User {
@@ -121,6 +123,67 @@ fn main() {
 }
 
 #[tauri::command]
+fn generate_random_color() -> String {
+    let color = RandomColor::new()
+        .luminosity(Luminosity::Light) // Ensuring the color is light, for a pastel-like effect
+        .alpha(0.2)
+        .to_hex()
+        .to_string(); // Output as an HSL string for finer control over the appearance
+
+    color
+}
+
+// *MAL API* //
+
+// ! DONT TOUCH THESE FUNCTIONS ! //
+#[tauri::command]
+fn show_in_folder(path: String) {
+    #[cfg(target_os = "windows")]
+    {
+        Command::new("explorer")
+            .args(["/select,", &path]) // The comma after select is not a typo
+            .spawn()
+            .unwrap();
+    }
+
+    #[cfg(target_os = "linux")]
+    {
+        if path.contains(",") {
+            // see https://gitlab.freedesktop.org/dbus/dbus/-/issues/76
+            let new_path = match metadata(&path).unwrap().is_dir() {
+                true => path,
+                false => {
+                    let mut path2 = PathBuf::from(path);
+                    path2.pop();
+                    path2.into_os_string().into_string().unwrap()
+                }
+            };
+            Command::new("xdg-open").arg(&new_path).spawn().unwrap();
+        } else {
+            if let Ok(Fork::Child) = daemon(false, false) {
+                Command::new("dbus-send")
+                    .args([
+                        "--session",
+                        "--dest=org.freedesktop.FileManager1",
+                        "--type=method_call",
+                        "/org/freedesktop/FileManager1",
+                        "org.freedesktop.FileManager1.ShowItems",
+                        format!("array:string:\"file://{path}\"").as_str(),
+                        "string:\"\"",
+                    ])
+                    .spawn()
+                    .unwrap();
+            }
+        }
+    }
+
+    #[cfg(target_os = "macos")]
+    {
+        Command::new("open").args(["-R", &path]).spawn().unwrap();
+    }
+}
+
+#[tauri::command]
 async fn open_video(path: String, handle: tauri::AppHandle) -> String {
     let window: Window = handle
         .clone()
@@ -212,62 +275,4 @@ async fn open_video(path: String, handle: tauri::AppHandle) -> String {
         // Sleep for a bit before checking again to reduce CPU usage.
         std::thread::sleep(std::time::Duration::from_millis(500));
     }
-}
-
-#[tauri::command]
-fn show_in_folder(path: String) {
-    #[cfg(target_os = "windows")]
-    {
-        Command::new("explorer")
-            .args(["/select,", &path]) // The comma after select is not a typo
-            .spawn()
-            .unwrap();
-    }
-
-    #[cfg(target_os = "linux")]
-    {
-        if path.contains(",") {
-            // see https://gitlab.freedesktop.org/dbus/dbus/-/issues/76
-            let new_path = match metadata(&path).unwrap().is_dir() {
-                true => path,
-                false => {
-                    let mut path2 = PathBuf::from(path);
-                    path2.pop();
-                    path2.into_os_string().into_string().unwrap()
-                }
-            };
-            Command::new("xdg-open").arg(&new_path).spawn().unwrap();
-        } else {
-            if let Ok(Fork::Child) = daemon(false, false) {
-                Command::new("dbus-send")
-                    .args([
-                        "--session",
-                        "--dest=org.freedesktop.FileManager1",
-                        "--type=method_call",
-                        "/org/freedesktop/FileManager1",
-                        "org.freedesktop.FileManager1.ShowItems",
-                        format!("array:string:\"file://{path}\"").as_str(),
-                        "string:\"\"",
-                    ])
-                    .spawn()
-                    .unwrap();
-            }
-        }
-    }
-
-    #[cfg(target_os = "macos")]
-    {
-        Command::new("open").args(["-R", &path]).spawn().unwrap();
-    }
-}
-
-#[tauri::command]
-fn generate_random_color() -> String {
-    let color = RandomColor::new()
-        .luminosity(Luminosity::Light) // Ensuring the color is light, for a pastel-like effect
-        .alpha(0.2)
-        .to_hex()
-        .to_string(); // Output as an HSL string for finer control over the appearance
-
-    color
 }
--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -302,7 +302,7 @@ export default function Settings() {
                                                     </TooltipProvider>
                                                     {hasMultipleProfiles && (
                                                         <TooltipProvider>
-                                                            <Tooltip delayDuration={500}>
+                                                            <Tooltip delayDuration={700}>
                                                                 <TooltipTrigger asChild className='flex w-full cursor-pointer flex-row items-center justify-center text-base'>
                                                                     <Button variant="destructive" className={cn('select-none w-3/4 py-[1px] h-fit  flex flex-row justify-center items-center rounded-sm gap-1 pb-1',
                                                                         formState?.fontSize === "Medium" && 'text-lg',
@@ -348,10 +348,38 @@ export default function Settings() {
                                                                 </TooltipContent>
                                                             </Tooltip>
                                                         </TooltipProvider>
-
-
-
                                                     )}
+                                                    <TooltipProvider>
+                                                        <Tooltip delayDuration={700}>
+                                                            <TooltipTrigger asChild className='flex w-full cursor-pointer flex-row items-center justify-center text-base'>
+                                                                <Button variant="secondary" className={cn('select-none w-3/4 py-[1px] h-fit  flex flex-row justify-center items-center rounded-sm gap-1 pb-1 bg-blue-500 text-white transition-colors duration-200',
+                                                                    formState?.fontSize === "Medium" && 'text-lg',
+                                                                    formState?.fontSize === "Large" && 'text-xl',
+                                                                    formState?.fontSize === "XLarge" && 'text-2xl',
+                                                                )} onClick={() => {
+                                                                    // invoke to the backend to link the user to MAL  
+                                                                }}>
+                                                                    Link to MAL
+                                                                </Button>
+                                                            </TooltipTrigger>
+                                                            <TooltipContent align='center' side='bottom' className={cn('select-none flex gap-1',
+                                                                formState?.fontSize === "Medium" && 'text-md',
+                                                                formState?.fontSize === "Large" && 'text-lg',
+                                                                formState?.fontSize === "XLarge" && 'text-xl',
+                                                            )}>
+                                                                <div className='font-medium'>
+                                                                    <div className='flex flex-col items-center justify-center gap-1'>
+                                                                        <div className='flex flex-row gap-0.5'>
+                                                                            <span className='rounded-sm font-bold'> Link your profile to</span>
+                                                                            <b className='cursor-pointer text-blue-600 text-primary underline transition-colors duration-200 hover:text-blue-900'>
+                                                                                MyAnimeList
+                                                                            </b>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </TooltipContent>
+                                                        </Tooltip>
+                                                    </TooltipProvider>
                                                 </div>
                                             </>
                                         )}
