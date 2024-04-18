27b37c31d60b53e6f77e67ec5c8949ef58f865a8
aramrw <106574385+aramrw@users.noreply.github.com>
2024-04-17 21:44:52 -0700

    refactor mpv functions

---
+++ src-tauri/src/mpv.rs
@@ -0,0 +1,200 @@
+use sqlx::{Connection, SqliteConnection};
+use std::time::Duration;
+use window_titles::{Connection as win_titles_Connection, ConnectionTrait};
+use tauri::{Window, Manager};
+use std::process::Command;
+use std::fs;
+use std::path::Path;
+use crate::misc::extract_episode_number;
+
+
+#[tauri::command]
+pub async fn open_video(
+    path: String,
+    handle: tauri::AppHandle,
+    auto_play: String,
+    user_id: u32,
+) -> String {
+    println!("now playing {}", path);
+
+    let window: Window = handle
+        .clone()
+        .get_window("main")
+        .or_else(|| handle.clone().get_window("main"))
+        .expect("failed to get any windows!");
+
+        window.close().expect("failed to close main window");
+
+    // Kill all mpv.exe processes before opening a new video.
+    let mut sys = sysinfo::System::new_all();
+    sys.refresh_processes(); // Refresh the list of processes.
+    sys.processes().iter().for_each(|(_pid, process)| {
+        if process.name().to_lowercase().contains("mpv.exe") {
+            process.kill();
+        }
+    });
+
+    let parent_path = Path::new(&path).parent().unwrap().to_str().unwrap();
+
+    // find which index the video the user clicked is in the parent folder
+    let mut current_video_index: i32 = -1;
+    let video_files = fs::read_dir(parent_path).unwrap();
+    for (index, file) in video_files.enumerate() {
+        let file = file.unwrap().path();
+        if file.to_str().unwrap() == path {
+            current_video_index = index as i32;
+            println!("current video index: {}", current_video_index);
+            break;
+        }
+   }
+
+    if auto_play == "On" {
+        let _status = Command::new("mpv.exe")
+            // --playlist-start should be the index of the video in the parent folder
+            .arg(format!("--playlist-start={}", current_video_index))
+            // --playlist should be the path of the parent folder
+            .arg(format!("--playlist={}", parent_path))
+            .spawn()
+            .unwrap();
+    } else {
+        match open::that(path.clone()) {
+            Ok(_) => "Video opened successfully",
+            Err(_) => "Failed to open video",
+        };
+    }
+
+    let instant = std::time::Instant::now();
+    let mut last_watched_video = String::new();
+
+    loop {
+        let mut mpv_running = false; // Flag to check if mpv is running.
+        sys.refresh_processes();
+
+        sys.processes().iter().for_each(|(_pid, process)| {
+            if process.name().to_lowercase().contains("mpv.exe") {
+                last_watched_video = get_last_mpv_win_title();
+                mpv_running = true;
+            }
+        });
+
+        if !mpv_running {
+            println!(
+                "mpv-shelf was running for {:.2} seconds in the background",
+                &instant.elapsed().as_secs_f32()
+            );
+
+            // open a new window and close the first exe (not a window anymore) in the system tray
+            match handle.get_window("main") {
+                Some(window) => {
+                    window.center().unwrap();
+                    window.show().unwrap();
+                    window.set_focus().unwrap();
+                }
+                None => {
+                    tauri::WindowBuilder::new(
+                        &handle,
+                        "main".to_string(),
+                        tauri::WindowUrl::App("/dashboard".into()),
+                    )
+                    .center()
+                    .transparent(true)
+                    .title("mpv-shelf")
+                    .inner_size(800.0, 600.0)
+                    .build()
+                    .unwrap();
+
+                    update_last_watched_videos(handle, path.clone(), last_watched_video, user_id)
+                        .await;
+
+                    return "closed".to_string();
+                }
+            }
+        }
+
+        tokio::time::sleep(Duration::from_secs(1)).await;
+    }
+}
+
+fn get_last_mpv_win_title() -> String {
+    let connection = win_titles_Connection::new().unwrap();
+    let mut current_episode = String::new();
+
+    connection
+        .window_titles()
+        .unwrap()
+        .iter()
+        .for_each(|window| {
+            if window.contains("mpv")
+                && window.contains('.')
+                && !window.contains('\\')
+                && !window.contains("Visual Studio")
+            {
+                println!("{}", window);
+                current_episode = window.to_string();
+            }
+        });
+
+    current_episode
+}
+
+async fn update_last_watched_videos(
+    handle: tauri::AppHandle,
+    video_start_path: String,
+    last_video_watched_title: String,
+    user_id: u32,
+) {
+    let video_start_title = Path::new(&video_start_path)
+        .file_name()
+        .unwrap()
+        .to_str()
+        .unwrap();
+
+    println!("last video watched: {}", last_video_watched_title);
+    let video_start_episode_num: u32 = extract_episode_number(video_start_title).unwrap_or(0);
+    let last_video_episode_num: u32 =
+        extract_episode_number(&last_video_watched_title).unwrap_or(0);
+    let mut sum: u32 = 0;
+
+    if video_start_episode_num == 0 && last_video_episode_num == 0 {
+        return;
+    }
+
+    if video_start_episode_num != last_video_episode_num {
+        sum = last_video_episode_num - video_start_episode_num;
+    }
+
+    let db_url = handle
+        .path_resolver()
+        .app_data_dir()
+        .unwrap()
+        .join("main.db");
+
+    let mut conn = SqliteConnection::connect(db_url.to_str().unwrap())
+        .await
+        .unwrap();
+
+    for x in 1..=sum {
+        let video_index = x + video_start_episode_num;
+        let parts: Vec<&str> = video_start_path
+            .rsplitn(2, &video_start_episode_num.to_string())
+            .collect();
+        let starting_new_path = if parts.len() == 2 {
+            parts[1].to_owned() + &video_index.to_string() + parts[0]
+        } else {
+            video_start_path.clone()
+        };
+
+        sqlx::query(
+            "INSERT OR REPLACE INTO video (path, userId, watched, lastWatchedAt) VALUES (?, ?, ?, datetime('now'))",
+        )
+        .bind(starting_new_path)
+        .bind(user_id)
+        .bind(true)
+        .execute(&mut conn)
+        .await
+        .unwrap();
+    }
+
+    conn.close().await.unwrap();
+}
+
