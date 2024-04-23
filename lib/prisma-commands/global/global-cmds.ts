79b9fbb78e6feda7dcd7b41811fa956ad9204c2c
aramrw <106574385+aramrw@users.noreply.github.com>
2024-04-22 23:57:37 -0700

    remove all create tables from prisma commands

--- lib/prisma-commands/global/global-cmds.ts
+++ lib/prisma-commands/global/global-cmds.ts
@@ -1,65 +1,52 @@
-import Database from "tauri-plugin-sql-api"
+import Database from "tauri-plugin-sql-api";
 import { Global } from "@prisma/client";
 
-
 export async function setCurrentUserGlobal({ userId }: { userId: number }) {
-    const db = await Database.load("sqlite:main.db");
+  const db = await Database.load("sqlite:main.db");
 
-    // Use a constant ID since there will only ever be one record in this table.
-    const GLOBAL_ID = 'GID99844589388427';
+  // Use a constant ID since there will only ever be one record in this table.
+  const GLOBAL_ID = "GID99844589388427";
 
-    try {
-
-        // Ensure the global table exists
-        await db.execute(`
-            CREATE TABLE IF NOT EXISTS global (
-                id TEXT PRIMARY KEY, 
-                userId INTEGER NOT NULL
-            )
-        `);
-
-        // Set the current user. The ON CONFLICT clause is used to upsert the userId for the constant ID.
-        await db.execute(`
+  try {
+    // Set the current user. The ON CONFLICT clause is used to upsert the userId for the constant ID.
+    await db.execute(
+      `
             INSERT INTO global (id, userId) 
             VALUES ($1, $2)
             ON CONFLICT(id) DO UPDATE SET 
             userId = excluded.userId
-        `, [GLOBAL_ID, userId]);
+        `,
+      [GLOBAL_ID, userId],
+    );
+  } catch (e) {
+    await db.close();
+    // console.log (e);
+    return false;
+  }
 
-    } catch (e) {
-        await db.close();
-        // console.log (e);
-        return false
-    }
-
-    return true
+  return true;
 }
 
 export async function getCurrentUserGlobal() {
-    const db = await Database.load("sqlite:main.db");
+  const db = await Database.load("sqlite:main.db");
 
-    const GLOBAL_ID = 'GID99844589388427';
+  const GLOBAL_ID = "GID99844589388427";
 
-    try {
-        // Ensure the global table exists
-        await db.execute(`
-        CREATE TABLE IF NOT EXISTS global (
-        id TEXT PRIMARY KEY, 
-        userId INTEGER NOT NULL
-        )`);
+  try {
+    const GLOBAL_USER: Global[] = await db.select(
+      "SELECT * from global WHERE id = $1",
+      [GLOBAL_ID],
+    );
 
-        const GLOBAL_USER: Global[] = await db.select("SELECT * from global WHERE id = $1", [GLOBAL_ID])
-
-        if (GLOBAL_USER) {
-            //await db.close();
-            return GLOBAL_USER[0];
-        }
-
-        return null;
-    } catch (e) {
-        await db.close();
-        // console.log (e);
-        return null;
+    if (GLOBAL_USER) {
+      //await db.close();
+      return GLOBAL_USER[0];
     }
 
+    return null;
+  } catch (e) {
+    await db.close();
+    // console.log (e);
+    return null;
+  }
 }
--- lib/prisma-commands/videos/video-cmds.ts
+++ lib/prisma-commands/videos/video-cmds.ts
@@ -1,84 +1,76 @@
-import Database from "tauri-plugin-sql-api"
+import Database from "tauri-plugin-sql-api";
 import { Video } from "@prisma/client";
 
 export async function getVideo({
-    videoPath,
-    userId
+  videoPath,
+  userId,
 }: {
-    videoPath: string,
-    userId: number
+  videoPath: string;
+  userId: number;
 }) {
+  let db = await Database.load("sqlite:main.db");
 
+  let video: Video[] = [];
 
-    let db = await Database.load("sqlite:main.db");
+  try {
+    video = await db.select(
+      "SELECT * from video WHERE path = $1 AND userId = $2",
+      [videoPath, userId],
+    );
+    // console.log  ("retrived", video[0].path.split("\\").pop(), "as", video[0].watched, "for user", video[0].userId);
+    return video[0];
+  } catch (e) {
+    // console.log (e);
+  }
 
-    let video: Video[] = [];
-
-    try {
-        // Ensure the video table exists with the correct schema including the userId field
-        await db.execute(`
-            CREATE TABLE IF NOT EXISTS video (
-                id INTEGER PRIMARY KEY AUTOINCREMENT, 
-                path TEXT NOT NULL, 
-                userId INTEGER,
-                watched BOOLEAN NOT NULL DEFAULT 0,
-                lastWatchedAt TIMESTAMP DEFAULT (datetime('now')),
-                FOREIGN KEY (userId) REFERENCES user(id)
-            )
-        `);
-
-        video = await db.select("SELECT * from video WHERE path = $1 AND userId = $2", [videoPath, userId])
-        // console.log  ("retrived", video[0].path.split("\\").pop(), "as", video[0].watched, "for user", video[0].userId);
-        return video[0];
-    } catch (e) {
-        // console.log (e);
-
-    }
-
-    if (video && video.length !== 0) {
-        // console.log  ("video", video);
-        return video[0];
-    } else {
-        return null
-    }
+  if (video && video.length !== 0) {
+    // console.log  ("video", video);
+    return video[0];
+  } else {
+    return null;
+  }
 }
 
 export async function unwatchVideo({
-    videoPath,
-    userId
+  videoPath,
+  userId,
 }: {
-    videoPath: string,
-    userId: number
+  videoPath: string;
+  userId: number;
 }) {
-    let db = await Database.load("sqlite:main.db");
+  let db = await Database.load("sqlite:main.db");
 
-    // console.log ("updating" + videoPath.split("\\").pop(), "as", "unwatched", "for user", userId, "in database");
+  // console.log ("updating" + videoPath.split("\\").pop(), "as", "unwatched", "for user", userId, "in database");
 
-    await db.execute("UPDATE video SET watched = 0 WHERE path = $1 AND userId = $2", [videoPath, userId]).catch((e) => {
-        // console.log ("error", e);
+  await db
+    .execute("UPDATE video SET watched = 0 WHERE path = $1 AND userId = $2", [
+      videoPath,
+      userId,
+    ])
+    .catch((e) => {
+      // console.log ("error", e);
     });
 
-    return true;
+  return true;
 }
 
-export async function userGetAllVideos({
-    userId
-}: {
-    userId: number
-}) {
-    const db = await Database.load("sqlite:main.db");
+export async function userGetAllVideos({ userId }: { userId: number }) {
+  const db = await Database.load("sqlite:main.db");
 
-    try {
-        const folders: Video[] = await db.select("SELECT * from video WHERE userId = $1", [userId])
+  try {
+    const folders: Video[] = await db.select(
+      "SELECT * from video WHERE userId = $1",
+      [userId],
+    );
 
-        if (folders.length !== 0) {
-            return folders;
-        } else {
-            return [];
-        }
-    } catch (e) {
-        // console.log (e);
-        await db.close();
-        return null;
+    if (folders.length !== 0) {
+      return folders;
+    } else {
+      return [];
     }
+  } catch (e) {
+    // console.log (e);
+    await db.close();
+    return null;
+  }
 }
