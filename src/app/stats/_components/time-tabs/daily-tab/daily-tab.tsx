8ed0107efc94b92e211ea5cc55450c453200bcd4
aramrw <106574385+aramrw@users.noreply.github.com>
2024-04-27 18:05:07 -0700

    remove daily tab content

--- src/app/stats/_components/time-tabs/daily-tab/daily-tab.tsx
+++ src/app/stats/_components/time-tabs/daily-tab/daily-tab.tsx
@@ -8,9 +8,6 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { invoke } from "@tauri-apps/api/tauri";
-import { useEffect, useState } from "react";
-import MangaCard from "../../manga-card";
 
 export default function DailyTab() {
   return (
@@ -23,11 +20,13 @@ export default function DailyTab() {
       </CardHeader>
       <CardContent className="space-y-1">
         <h1 className="text-sm font-bold">Updated Today</h1>
-        <div className="py-1 max-h-56 overflow-auto max-w-52 rounded-md flex flex-col justify-self-auto items-start gap-2 shadow-md outline outline-secondary">
-          {/*dailyMangaFolders.map((manga, index) => (
-            <MangaCard key={index} mangaFolder={manga} />
-          ))*/}
-        </div>
+        {/*
+  <div className="py-1 max-h-56 overflow-auto max-w-52 rounded-md flex flex-col justify-self-auto items-start gap-2 shadow-md outline outline-secondary">
+    dailyMangaFolders.map((manga, index) => (
+      <MangaCard key={index} mangaFolder={manga} />
+    ))
+  </div>
+  */}
       </CardContent>
       <CardFooter></CardFooter>
     </Card>
