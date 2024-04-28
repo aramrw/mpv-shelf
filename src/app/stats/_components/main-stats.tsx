907493ce29b186eae24d6588823e198b4c6e3960
aramrw <106574385+aramrw@users.noreply.github.com>
2024-04-27 17:19:51 -0700

    fix ui styling

--- src/app/stats/_components/main-stats.tsx
+++ src/app/stats/_components/main-stats.tsx
@@ -48,36 +48,36 @@ export default function MainStats() {
   return (
     <div className="w-full flex flex-col justify-center items-center h-fit bg-card rounded-xl shadow-md outline outline-border p-2">
       <h1 className="font-bold rounded-md px-1">Main Stats</h1>
-      <Separator className="h-[1.1px] w-1/5 mb-1.5 mr-0.5" />
-      <ul className="flex flex-row justify-center items-start gap-3 pb-0.5">
+      <Separator className="h-[2px] w-1/5 my-1.5 mr-0.5" />
+      <ul className="w-full flex flex-row justify-center items-start gap-3 pb-0.5">
         <li className="flex flex-col justify-center items-start gap-0.5 text-xs bg-muted px-2 pb-1.5 pt-0.5 rounded-md">
           <label className="font-semibold underline">Total Anime</label>
           <span className="font-medium bg-accent rounded-sm px-0.5">
             {mainStats?.total_anime}
           </span>
         </li>
-        <Separator className="w-[1.1px] h-11" />
+        <Separator className="min-w-[1.5px] max-w-[1.9px] h-11" />
         <li className="flex flex-col justify-center items-start gap-0.5 text-xs bg-muted px-2 pb-1.5 pt-0.5 rounded-md">
           <label className="font-semibold underline">Total Videos</label>
           <span className="font-medium bg-accent rounded-sm px-0.5">
             {mainStats?.total_videos}
           </span>
         </li>
-        <Separator className="w-[1px] h-11" />
+        <Separator className="min-w-[1.9px] max-w-[2.05px] h-11" />
         <li className="flex flex-col justify-center items-start gap-0.5 text-xs bg-muted px-2 pb-1.5 pt-0.5 rounded-md">
           <label className="font-semibold underline">Videos Watched</label>
           <span className="font-medium bg-accent rounded-sm px-0.5">
             {mainStats?.videos_watched}
           </span>
         </li>
-        <Separator className="w-[1.1px] h-11" />
+        <Separator className="min-w-[2.1px] max-w-[2px] h-11" />
         <li className="flex flex-col justify-center items-start gap-0.5 text-xs bg-muted px-2 pb-1.5 pt-0.5 rounded-md">
           <label className="font-semibold underline">Videos Remaining</label>
           <span className="font-medium bg-accent rounded-sm px-0.5">
             {mainStats?.videos_remaining}
           </span>
         </li>
-        <Separator className="w-[1.1px] h-11" />
+        <Separator className="min-w-[2.1px] max-w-[2px] h-11" />
         <li className="flex flex-col justify-center items-start gap-0.5 text-xs bg-muted px-2 pb-1.5 pt-0.5 rounded-md">
           <label className="font-semibold underline">Watch Time</label>
           <span className="font-medium bg-accent rounded-sm px-0.5"></span>
