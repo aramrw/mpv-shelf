aae414467049f50201741db1582b9329dbef440b
aramrw <106574385+aramrw@users.noreply.github.com>
2024-04-27 17:05:07 -0700

    Create daily-tab.tsx

---
+++ src/app/stats/_components/time-tabs/daily-tab/daily-tab.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { invoke } from "@tauri-apps/api/tauri";
+import { useEffect, useState } from "react";
+import MangaCard from "../../manga-card";
+
+export default function DailyTab() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Daily</CardTitle>
+        <CardDescription>
+          Set goals & track your daily reading progress here.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-1">
+        <h1 className="text-sm font-bold">Updated Today</h1>
+        <div className="py-1 max-h-56 overflow-auto max-w-52 rounded-md flex flex-col justify-self-auto items-start gap-2 shadow-md outline outline-secondary">
+          {/*dailyMangaFolders.map((manga, index) => (
+            <MangaCard key={index} mangaFolder={manga} />
+          ))*/}
+        </div>
+      </CardContent>
+      <CardFooter></CardFooter>
+    </Card>
+  );
+}
