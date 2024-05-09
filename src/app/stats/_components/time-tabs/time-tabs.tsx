620fb31ac17541dfad6c57b538304f15c17ca950
aramrw <106574385+aramrw@users.noreply.github.com>
2024-05-09 12:29:01 -0700

    update time tabs to match

--- src/app/stats/_components/time-tabs/time-tabs.tsx
+++ src/app/stats/_components/time-tabs/time-tabs.tsx
@@ -1,25 +1,25 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import WeeklyTab from "./weekly-tab/weekly-tab";
 import MonthlyTab from "./monthly-tab/monthly-tab";
-import YearlyTab from "./yearly-tab/yearly-tab";
+import DailyTab from "./daily-tab/daily-tab";
 
 export default function TimeTabs() {
   return (
     <div className="w-full h-fit flex flex-row justify-center items-center">
       <Tabs defaultValue="weekly" className="w-full">
         <TabsList className="grid w-full grid-cols-3">
-          <TabsTrigger value="weekly">Weekly</TabsTrigger>
-          <TabsTrigger value="monthly">Monthly</TabsTrigger>
-          <TabsTrigger value="yearly">Yearly</TabsTrigger>
+          <TabsTrigger value="weekly">Daily</TabsTrigger>
+          <TabsTrigger value="monthly">Weekly</TabsTrigger>
+          <TabsTrigger value="yearly">Monthly</TabsTrigger>
         </TabsList>
         <TabsContent value="weekly">
-          <WeeklyTab />
+          <DailyTab />
         </TabsContent>
         <TabsContent value="monthly">
-          <MonthlyTab />
+          <WeeklyTab />
         </TabsContent>
         <TabsContent value="yearly">
-          <YearlyTab />
+          <MonthlyTab />
         </TabsContent>
       </Tabs>
     </div>
