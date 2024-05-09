92d75480c7fefc3491c166c256722cbdb2062b44
aramrw <106574385+aramrw@users.noreply.github.com>
2024-05-09 12:21:07 -0700

    update time tabs

--- src/app/stats/_components/time-tabs/time-tabs.tsx
+++ src/app/stats/_components/time-tabs/time-tabs.tsx
@@ -1,23 +1,17 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import DailyTab from "./daily-tab/daily-tab";
 import WeeklyTab from "./weekly-tab/weekly-tab";
 import MonthlyTab from "./monthly-tab/monthly-tab";
 import YearlyTab from "./yearly-tab/yearly-tab";
-import { Lock } from "lucide-react";
 
 export default function TimeTabs() {
   return (
     <div className="w-full h-fit flex flex-row justify-center items-center">
-      <Tabs defaultValue="daily" className="w-full">
-        <TabsList className="grid w-full grid-cols-4">
-          <TabsTrigger value="daily">Daily</TabsTrigger>
+      <Tabs defaultValue="weekly" className="w-full">
+        <TabsList className="grid w-full grid-cols-3">
           <TabsTrigger value="weekly">Weekly</TabsTrigger>
           <TabsTrigger value="monthly">Monthly</TabsTrigger>
           <TabsTrigger value="yearly">Yearly</TabsTrigger>
         </TabsList>
-        <TabsContent value="daily">
-          <DailyTab />
-        </TabsContent>
         <TabsContent value="weekly">
           <WeeklyTab />
         </TabsContent>
