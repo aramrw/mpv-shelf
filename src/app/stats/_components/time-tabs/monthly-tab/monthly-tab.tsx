10a677ab991bc064a44b8d4947464c877e74af07
aramrw <106574385+aramrw@users.noreply.github.com>
2024-05-09 12:20:26 -0700

    add weekly tab chart

--- src/app/stats/_components/time-tabs/monthly-tab/monthly-tab.tsx
+++ src/app/stats/_components/time-tabs/monthly-tab/monthly-tab.tsx
@@ -1,23 +1,82 @@
+"use client";
+
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
+  //CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { invoke } from "@tauri-apps/api/tauri";
+import "chart.js/auto";
+import { useEffect, useState } from "react";
+import { Bar } from "react-chartjs-2";
 
 export default function MonthlyTab() {
+  let now = new Date();
+  let month = now.getMonth(); // get the current month
+  let year = now.getFullYear(); // get the current year
+  let daysInMonth = new Date(year, month + 1, 0).getDate();
+
+  const labels = Array.from({ length: daysInMonth }, (_, i) =>
+    (i + 1).toString(),
+  );
+
+  const [stats, setStats] = useState<number[]>();
+
+  useEffect(() => {
+    invoke("create_chart_stats", { range: "monthly", daysInMonth: labels.length }).then((daily) => {
+      console.log(daily);
+      setStats(daily as number[]);
+    });
+  }, []);
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: " Watchtime Per Day (H) ",
+        data: stats,
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(255, 205, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(201, 203, 207, 0.2)",
+        ],
+        borderColor: [
+          "rgb(255, 99, 132)",
+          "rgb(255, 159, 64)",
+          "rgb(255, 205, 86)",
+          "rgb(75, 192, 192)",
+          "rgb(54, 162, 235)",
+          "rgb(153, 102, 255)",
+          "rgb(201, 203, 207)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Monthly</CardTitle>
-        <CardDescription>
-          Track your monthly reading progress here.
+        <CardTitle className="pointer-events-none select-none">
+          Monthly
+        </CardTitle>
+        <CardDescription className="underline pointer-events-none select-none">
+          Last Updated : {new Date().toLocaleDateString()} @{" "}
+          {new Date().toLocaleTimeString()}{" "}
         </CardDescription>
       </CardHeader>
-      <CardContent className="space-y-2"></CardContent>
-      <CardFooter></CardFooter>
+      <CardContent className="space-y-1">
+        <div className="w-fit h-fit">
+          <Bar data={data} />
+        </div>
+      </CardContent>
     </Card>
   );
 }
