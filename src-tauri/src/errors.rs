8c011b69cfd403f7aee5eda4a3d3b95afdb3a3bb
aramrw <106574385+aramrw@users.noreply.github.com>
2024-06-25 18:54:55 -0700

    define BackUpDataErrors

--- src-tauri/src/errors.rs
+++ src-tauri/src/errors.rs
@@ -3,3 +3,17 @@ use std::sync::mpsc;
 
 use thiserror::Error;
 
+#[derive(Error, Debug)]
+pub enum BackupDataErrors {
+    // #[error("Err: Failed to Export Stats: {0}")]
+    // Export(String),
+    #[error("DB Err: {0}")]
+    DB(#[from] sqlx::Error),
+    #[error("JSON Err: {0}")]
+    Json(#[from] serde_json::Error),
+    #[error("IO Err: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("Channel Err: {0}")]
+    Channels(#[from] mpsc::RecvError),
+}
+
