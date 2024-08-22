154ba689b226ba127624959067aec06a264a1132
aramrw <106574385+aramrw@users.noreply.github.com>
2024-08-21 23:24:40 -0700

    update video-contex-menu to use custom eye icons

--- src/app/dashboard/_components/_video-files/video-context-menu.tsx
+++ src/app/dashboard/_components/_video-files/video-context-menu.tsx
@@ -6,6 +6,9 @@ import { SettingSchema } from '@/app/settings/page'
 import { FileEntry } from '@tauri-apps/api/fs'
 import type { User, } from "@prisma/client";
 import { invoke } from '@tauri-apps/api/tauri'
+import EyeIcon from '@/app/_main-components/_icon_components/eye-icon'
+import CustomEyeIcon from '@/app/_main-components/_icon_components/eye-icon'
+import CustomEyeOffIcon from '@/app/_main-components/_icon_components/eye-off-icon'
 
 export default function VideoContextMenu({ userSettings, file, currentUser, index, handleUnwatchVideo, handleCheckWatched, handleWatchVideo, handleSliceToWatchVideo, handleSliceToUnwatchVideo }:
   { userSettings: SettingSchema | undefined, file: FileEntry, currentUser: User | undefined, index: number, handleUnwatchVideo: (file: FileEntry) => void, handleCheckWatched: (file: FileEntry) => boolean, handleWatchVideo: (file: FileEntry) => void, handleSliceToWatchVideo: (index: number) => void, handleSliceToUnwatchVideo: (index: number) => void }) {
@@ -17,8 +20,7 @@ export default function VideoContextMenu({ userSettings, file, currentUser, inde
       onClick={(e) => {
         e.stopPropagation();
         if (e.button === 0) {
-          invoke('show_in_folder', { path: `${file.path}` }).then((res) => {
-          })
+          invoke('show_in_folder', { path: `${file.path}` });
         }
       }}
     >
@@ -35,12 +37,13 @@ export default function VideoContextMenu({ userSettings, file, currentUser, inde
     </ContextMenuItem>
     <ContextMenuSeparator className='my-1 h-[1px] bg-accent' />
     <ContextMenuSub>
-      <ContextMenuSubTrigger className={cn('cursor-pointer gap-1 font-medium',
+      <ContextMenuSubTrigger className={cn('gap-1 font-medium',
         userSettings?.fontSize === "Medium" && 'text-base',
         userSettings?.fontSize === "Large" && 'text-lg',
         userSettings?.fontSize === "XLarge" && 'text-xl'
       )}
-        inset>Watch</ContextMenuSubTrigger>
+        inset>Watch
+      </ContextMenuSubTrigger>
       <ContextMenuSubContent className="mx-2 overflow-hidden rounded-md border bg-popover p-1 font-medium text-popover-foreground shadow-md animate-in fade-in-80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2">
         {handleCheckWatched(file) ? (
           <ContextMenuItem
@@ -56,12 +59,7 @@ export default function VideoContextMenu({ userSettings, file, currentUser, inde
               userSettings?.fontSize === "XLarge" && 'text-xl'
             )}>Set Watched</span>
 
-            <Eye className={cn('h-auto w-4 ',
-              userSettings?.fontSize === "Medium" && 'h-auto w-5',
-              userSettings?.fontSize === "Large" && 'h-auto w-6',
-              userSettings?.fontSize === "XLarge" && 'h-auto w-7'
-            )} />
-
+            <CustomEyeIcon userSettings={userSettings} />
           </ContextMenuItem>
         ) : (
           <ContextMenuItem className='flex cursor-pointer gap-1'
@@ -78,11 +76,7 @@ export default function VideoContextMenu({ userSettings, file, currentUser, inde
               userSettings?.fontSize === "XLarge" && 'text-xl'
             )}>Unwatch</span>
 
-            <EyeOff className={cn('h-auto w-4 ',
-              userSettings?.fontSize === "Medium" && 'h-auto w-5',
-              userSettings?.fontSize === "Large" && 'h-auto w-6',
-              userSettings?.fontSize === "XLarge" && 'h-auto w-7'
-            )} />
+            <CustomEyeOffIcon userSettings={userSettings} />
           </ContextMenuItem>
         )}
         <ContextMenuSeparator className='my-1 h-[1px] bg-accent' />
@@ -101,11 +95,7 @@ export default function VideoContextMenu({ userSettings, file, currentUser, inde
               userSettings?.fontSize === "XLarge" && 'text-xl'
             )}>Unwatch To</span>
             <div className='flex'>
-              <EyeOff className={cn('h-auto w-4 ',
-                userSettings?.fontSize === "Medium" && 'h-auto w-5',
-                userSettings?.fontSize === "Large" && 'h-auto w-6',
-                userSettings?.fontSize === "XLarge" && 'h-auto w-7'
-              )} />
+              <CustomEyeOffIcon userSettings={userSettings} />
               <ChevronUp className={cn('h-auto w-4 ',
                 userSettings?.fontSize === "Medium" && 'h-auto w-5',
                 userSettings?.fontSize === "Large" && 'h-auto w-6',
@@ -121,21 +111,16 @@ export default function VideoContextMenu({ userSettings, file, currentUser, inde
             if (currentUser) {
               handleSliceToWatchVideo(index)
             }
-
           }}
         >
-          <div className='flex gap-1'>
+          <div className='flex gap-1 w-full justify-between'>
             <span className={cn("",
               userSettings?.fontSize === "Medium" && 'text-base',
               userSettings?.fontSize === "Large" && 'text-lg',
               userSettings?.fontSize === "XLarge" && 'text-xl'
             )}>Watch To</span>
             <div className='flex'>
-              <Eye className={cn('h-auto w-4 ',
-                userSettings?.fontSize === "Medium" && 'h-auto w-5',
-                userSettings?.fontSize === "Large" && 'h-auto w-6',
-                userSettings?.fontSize === "XLarge" && 'h-auto w-7'
-              )} />
+              <CustomEyeIcon userSettings={userSettings} />
               <ChevronDown className={cn('h-auto w-4 ',
                 userSettings?.fontSize === "Medium" && 'h-auto w-5',
                 userSettings?.fontSize === "Large" && 'h-auto w-6',
