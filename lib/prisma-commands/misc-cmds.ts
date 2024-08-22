f8bc421d0a91315d4993647ecfb04605ed51fae2
aramrw <106574385+aramrw@users.noreply.github.com>
2024-08-21 23:18:06 -0700

    Update misc-cmds.ts

--- lib/prisma-commands/misc-cmds.ts
+++ lib/prisma-commands/misc-cmds.ts
@@ -5,53 +5,53 @@ import { invoke } from '@tauri-apps/api/tauri';
 
 // scroll hooks need to be moved to /hooks 
 export async function updateUserScrollY({
-    userId,
-    scrollY
+  userId,
+  scrollY
 }: {
-    userId: number,
-    scrollY: number
+  userId: number,
+  scrollY: number
 }) {
-    const db = await Database.load("sqlite:main.db");
+  const db = await Database.load("sqlite:main.db");
 
-    await db.execute("UPDATE user SET scrollY = $1 WHERE id = $2", [scrollY, userId])
+  await db.execute("UPDATE user SET scrollY = $1 WHERE id = $2", [scrollY, userId])
 }
 
 export async function getUserScrollY({
-    userId
+  userId
 }: {
-    userId: number
+  userId: number
 }) {
-    const db = await Database.load("sqlite:main.db");
+  const db = await Database.load("sqlite:main.db");
 
-    try {
-        const scrollY: any = await db.select("SELECT scrollY from user WHERE id = $1", [userId])
+  try {
+    const scrollY: any = await db.select("SELECT scrollY from user WHERE id = $1", [userId])
 
-        if (scrollY) {
-            return scrollY[0].scrollY;
-        }
-    } catch (e) {
-        // console.log (e);
-        await db.close();
-        return null;
+    if (scrollY) {
+      return scrollY[0].scrollY;
     }
+  } catch (e) {
+    // console.log (e);
+    await db.close();
+    return null;
+  }
 }
 
 export async function closeDatabase() {
-    const db = await Database.load("sqlite:main.db");
-    await db.close();
+  const db = await Database.load("sqlite:main.db");
+  await db.close();
 }
 
-export async function randomizeFolderColor({ folderPath }: { folderPath: string }) {
-    const db = await Database.load("sqlite:main.db");
+export async function randomizeFolderColor(folderPath: string, hue: string | undefined) {
+  const db = await Database.load("sqlite:main.db");
 
-    return invoke("generate_random_mono_color").then(async (color: any) => {
-        await db.execute("UPDATE folder SET color = $1 WHERE path = $2 ", [color, folderPath]);
-        return color;
-    });
+  return invoke("generate_random_color", { hue }).then(async (color: any) => {
+    await db.execute("UPDATE folder SET color = $1 WHERE path = $2 ", [color, folderPath]);
+    return color;
+  });
 }
 
 export async function getFolderColor({ folderPath }: { folderPath: string }) {
-    const db = await Database.load("sqlite:main.db");
+  const db = await Database.load("sqlite:main.db");
 
-    return await db.select("SELECT color FROM folder WHERE path = $1", [folderPath]);
+  return await db.select("SELECT color FROM folder WHERE path = $1", [folderPath]);
 }
