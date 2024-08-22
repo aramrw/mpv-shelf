593341ce73c3bdfa0d5727b6e5fe29358b4ea058
aramrw <106574385+aramrw@users.noreply.github.com>
2024-08-21 23:24:22 -0700

    Update reset-user-on-load.tsx

--- src/app/_main-components/reset-user-on-load.tsx
+++ src/app/_main-components/reset-user-on-load.tsx
@@ -8,27 +8,25 @@ import { useRouter } from 'next/navigation';
 
 export default function ResetUserOnLoad() {
 
-    let router = useRouter();
+  let router = useRouter();
 
-    useEffect(() => {
-        //console.log('resetting user on load');
-        //router.prefetch('/login');
+  useEffect(() => {
+    //console.log('resetting user on load');
+    //router.prefetch('/login');
+    closeDatabase().then(() => {
+      setCurrentUserGlobal({ userId: -1 }).then(() => {
         closeDatabase().then(() => {
-            setCurrentUserGlobal({ userId: -1 }).then(() => {
-                closeDatabase().then(() => {
-                    router.push('/login', { scroll: false });
-                });
+          router.push('/login', { scroll: false });
+        });
 
-            })
-        })
+      })
+    })
 
-    }, [router])
+  }, [router])
 
-
-
-    return (
-        <main className="flex h-1/2 w-full items-center justify-center">
-            <Loader2 className="animate-spin text-accent" size={140} />
-        </main>
-    )
+  return (
+    <main className="flex h-1/2 w-full items-center justify-center">
+      <Loader2 className="animate-spin text-accent" size={140} />
+    </main>
+  )
 }
