5dd4277430bbd6aa781e72cde8acc9f307309723
aramrw <106574385+aramrw@users.noreply.github.com>
2024-08-24 14:23:41 -0700

    Update footer.tsx

--- src/app/dashboard/_components/footer.tsx
+++ src/app/dashboard/_components/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function DashboardFooter() {
   return (
-    <div className='w-full h-20 bg-accent border border-t-2 border-zinc-300 drop-shadow-md'></div>
+    <div className='w-full h-56 bg-accent border border-t-2 border-zinc-300 drop-shadow-md'></div>
   )
 }
 
